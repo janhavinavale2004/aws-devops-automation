2a56e2e09beff9634c89e6e4e629d51cedb0cbf2
janhavi <janhavinavale2004@gmail.com>
2026-09-13 10:26:08 +0000

    Create Flask application with tests

---
+++ app/test_app.py
@@ -0,0 +1,26 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "running"
+    assert data["message"] == "AWS DevOps Automation Project"
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "healthy"
